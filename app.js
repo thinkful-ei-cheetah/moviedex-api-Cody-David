94e5dd834c4a81ef66bd24f6810577f5c98828c3
Cody Gillette <cgillette1312@gmail.com>
2019-04-30 12:09:39 -0700

    first one

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+const books = require('./book.js');
+
+app.get('/books', (req, res) => {
+  const { search = ' ', sort } = req.query;
+  if (sort) {
+    if (!['title', 'rank'].includes(sort)) {
+      return res
+        .status(400)
+        .send('sort must be one of title or rank');
+    }
+  }
+
+  let results = books
+    .filter(book =>
+      book
+        .title
+        .toLowerCase()
+        .includes(search.toLowerCase()));
+
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+  res
+    .json(results);
+});
+
+app.listen(8000, () => {
+  console.log('server started on port 8000');
+});
