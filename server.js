91bc5e1cb11895a0a6f924bd4a79666f16daf940
David Nordeen <davidnordeen@gmail.com>
2019-05-03 12:01:18 -0700

    yes

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
